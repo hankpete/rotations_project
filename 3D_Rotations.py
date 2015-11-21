596f1257c27f2894b8f01a5034f8eac36b0682bd
hankpete <hptrsn@comcast.net>
2015-11-20 16:50:57 -0800

    fixed distortions of image

--- 3D_Rotations.py
+++ 3D_Rotations.py
@@ -13,24 +13,26 @@ from mpl_toolkits.mplot3d import Axes3D			# part of ^this one
 
 # set up the two curves - these can be changed manually
 def function_a(x):
-	return x + 1
+	return (5*x + 1)
 
 def function_b(x, derivative):
 	if derivative:		# need this for when we make perpendicular lines
-		return (1)
+		return (.5)
 	else:
-		return (x)
+		return (.5*x)
 
 # set up original curves
 MIN = 0
 MAX = 2
-STEP = 10
-xvals = np.linspace(MIN, MAX, STEP)
+NUM = 10
+xvals = np.linspace(MIN, MAX, NUM)
 a_yvals = function_a(xvals)
 b_yvals = function_b(xvals, derivative=False)
 
 # plot what we have so far and set up for the rest
-fig = plt.figure()
+# my_dpi = 144
+# fig = plt.figure(figsize=(700/my_dpi, 700/my_dpi), dpi=my_dpi)
+fig =plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.plot(xvals, a_yvals, "b-")
 ax.plot(xvals, b_yvals, "r-")
@@ -47,19 +49,20 @@ for i in range(len(xvals)):
 
 	# get perpendicular slope to reflection point on curve B 
 	slope = function_b(b_x, derivative=True)
-	slope = -(slope)**(-1)
-	
+	slope = slope**(-1)
+	slope = -slope
+
 	# find which point on curve A lies on the line corresponding to above slope and point
 	x = Symbol('x')		# sympy's "Symbol" makes x a variable
-	a_x = solve((slope*(x - b_x) + b_y) - function_a(x), x)	# sympy's "solve" sets expression to zero
+	a_x = solve((slope*(x - b_x) + b_y) - (5*x +1), x)	# sympy's "solve" sets expression to zero
 	a_x = a_x[0]
 	if a_x < MIN or a_x > MAX:
 		continue
 	a_y = function_a(a_x)
 
 	# radius of circle is dist btwn point on line A and point on line B
-	delta_x = float(b_x-a_x)
-	delta_y = float(b_y-a_y)
+	delta_x = np.abs(float(b_x-a_x))
+	delta_y = np.abs(float(b_y-a_y))
 	radius = np.sqrt((delta_x)**2 + (delta_y)**2)
 
 	# make a line segment on xy plane that is circle's diameter
@@ -81,6 +84,14 @@ for i in range(len(xvals)):
 	circle_ys = np.repeat(circle_ys, 2)
 
 	# plot this particular circle and do the next one
-	ax.scatter(circle_xs, circle_ys, circle_zs, c="g")
+	ax.plot(circle_xs, circle_ys, circle_zs)
+	#ax.scatter(circle_xs, circle_ys, circle_zs, c="g")
 
+# make sure it's not distorted
+diff = MAX+2 - (MIN-2)
+ax.set_xlim(MIN-2, MAX+2)
+ax.set_ylim(MIN-2, MAX+2)
+ax.set_zlim(-diff/2.0, diff/2.0)
+
+# finish
 plt.show()
